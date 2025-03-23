c228d73875a2bb599b05d1c2af0d77fb16a0dbf7
Kashif <ahmed.kashif1001@gmail.com>
2025-03-23 23:09:50 +0500

    express backend setup

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 
 const app = express();
 
+const PORTT = 3000;
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+app.listen(PORTT, () => {
   console.log('Server is running on port 3000');
 });
