1afc9ffdce1afbc2dcb26e8b1ebdad3a0b7a3258
Kashif <ahmed.kashif1001@gmail.com>
2025-05-05 23:26:00 +0500

    fields added to schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -19,6 +19,18 @@ const userSchema = new mongoose.Schema({
     type: String,
     default:""
   },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export default mongoose.model('User', userSchema);
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
+//creating a new router instance
 const router = express.Router();
 
-router.post('/register', async(req, res) => {
+router.post('/register', async (req, res) => {
   res.send('Register route');
 });
 
-router.post('/login', async(req, res)=>{
+router.post('/login', async (req, res)=>{
   res.send('Login route');
 })
 
