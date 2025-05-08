c503ec128c141bf62822cbb389f9b14f3025389c
Kashif <ahmed.kashif1001@gmail.com>
2025-05-09 01:35:06 +0500

    book routes setup, bookSchema creation

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 
 import { connectDB } from "./lib/db.js"
 import authRoutes from './routes/authRoutes.js';
+import bookRoutes from './routes/bookRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(express.json()); //middleware to parse JSON bodies
 
 //mounts the authRoutes router as middleware to handle requests starting with "/api/auth"
 app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,40 +1,6 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
-
-router.post('/', (req, res) => {
-  try {
-    const { title, author, description, price, image } = req.body;
-
-    if(!title || !author || !description || !price || !image) {
-      return res.status(400).json({ message: 'Please provide all the required fields' });
-    }
-    const book = new Book({
-      title,
-      author,
-      description,
-      price,
-      isbn,
-      image,
-    });
-    book.save();
-
-    res.status(201).json(book);
-  } catch(err) {
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-//pagination => infinite loading
-router.get('/', async (req, res) => {
-  try{
-  const books = await Book.find().sort({ createdAt: -1 });
-  res.send(books);
-  } catch(err) {
-    res.status(500).json({ message: 'Internal server error' });
-  }
-})
-
 
 
 export default router;
