3a21c5b87fff4c72ff35d406a3be5f936daf9170
Kashif <ahmed.kashif1001@gmail.com>
2025-07-19 15:42:39 +0500

    added zustand for global state management

---
+++ mobile/store/authStore.js
@@ -0,0 +1,8 @@
+import { create } from "zustand";
+
+// whatever you return inside this object will be accessible from the other files this Zustand store provides a 'user' object and a 'sayHello' function. 'user' is accessible immediately when useAuthStore is used in a component. 'sayHello' only runs its code (console.log) when
+export const useAuthStore = create((set) => ({
+  user: { name: "john" },
+
+  sayHello: () => console.log("hello"),
+}));
